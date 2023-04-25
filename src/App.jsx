bd33160266c7babf507984907cafa5ae219207cb
Yasmin <souzasmimi@gmail.com>
2023-04-24 21:43:01 -0300

    teste React

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,22 @@ import { useState } from "react";
 
 function App() {
   const [tarefa,setTarefa] = useState("");
-  const [tarefas,setTarefas] = useState([{
-    id: 1,
-    tarefa: "Estudar React",
-    status: false
-    
-  }]);
+  const [tarefas,setTarefas] = useState([
+    {
+      id: 1,
+      tarefa: "Estudar React",
+      //status: false,
+      checked: true,
+    },
+    {
+      id: 2,
+      tarefa: "Estudar React",
+      //status: false,
+      checked: false,
+    }
+  ]);
+
+  console.log(tarefas);
 
   return (
     <Container>
@@ -18,14 +28,17 @@ function App() {
       </h1>
       <Spacer/>
       <Flex direction="row" justify="between">
-        <Input placeholder="Digite sua tarefa!"/>
+        <Input placeholder="Digite sua tarefa!" 
+          onChange={(e) => setTarefa(e.target.value)}/>
         <Button>Adicionar</Button>
       </Flex>
       <Spacer margin="16px"/>
       <ul>
-          <Item checked={true}>
+        {tarefas.map((tarefa) => (
+          <>
+          <Item checked={tarefa.checked} key={tarefa.id}>
             <p>
-              React
+              {tarefa.tarefa}
             </p>
             <Flex direction="row">
               <button>
@@ -35,22 +48,10 @@ function App() {
                 <i class="bx bx-trash"></i>
               </button>
             </Flex>
-          </Item>  
-          <Spacer margin="14px"/>
-          <Item>
-            <p>
-              React
-            </p>
-            <Flex direction="row">
-              <button>
-                <i class="bx bx-check-circle"></i>
-              </button>
-              <button>
-                <i class="bx bx-trash"></i>
-              </button>
-            </Flex>
-          </Item>  
-          <Spacer margin="12px"/>
+          </Item>
+          <Spacer margin="16px"/>
+          </>          
+        ))}
       </ul>
     </Container>
   );
